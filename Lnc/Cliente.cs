b8e14635e7360dee41c184391d28ff5274e72147
Vinicius Lima <ViniciusLimaCara@hotmail.com>
2021-09-20 16:34:10 -0300

    oiu

--- Lnc/Cliente.cs
+++ Lnc/Cliente.cs
@@ -28,6 +28,37 @@ namespace Lnc
             var strJson = JsonConvert.SerializeObject(cliente, Formatting.Indented);
             return SalvarArquivos(strJson, path);
         }
+        public static Cliente ListarClient(string path)
+        {
+            var strJson = OpenFileCliente(path);
+            if (strJson.Substring(0, 5) != "Falha")
+            {
+                return JsonConvert.DeserializeObject<Cliente>(strJson);
+            }
+            else
+            {
+                var cliente = new Cliente();
+                cliente.Nome = strJson;
+                return cliente;
+            }
+        }
+        public static List<Cliente> carregarCliente(string path)
+        {
+            var strJson = OpenFileCliente(path);
+            if (strJson.Substring(0, 5) != "Falha")
+            {
+                return JsonConvert.DeserializeObject<List<Cliente>>(strJson);
+            }
+            else
+            {
+                var listClientes = new List<Cliente>();
+                var cliente = new Cliente();
+                cliente.Nome = strJson;
+                listClientes.Add(cliente);
+                cliente.Nome = strJson;
+                return listClientes;
+            }
+        }
         public bool SalvarArquivos(string strJson, string path)
         {
             try
@@ -42,6 +73,23 @@ namespace Lnc
             {
                 MessageBox.Show("Falha" + ex.Message);
                 return false;
+            }
+        }
+
+        private static string OpenFileCliente(string path)
+        {
+            try
+            {
+                var strJson = "";
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    strJson = sr.ReadToEnd();
+                }
+                return strJson;
+            }
+            catch (Exception ex)
+            {
+                return "Falha" + ex.Message;
             }
         }
     }
